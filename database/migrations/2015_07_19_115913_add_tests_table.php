6e5760308a817bd2b7213d53e4e669f603426396
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2015-07-20 00:06:18 +1200

    Add tests table

---
+++ database/migrations/2015_07_19_115913_add_tests_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTestsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('tests', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('test_result_id')->unsigned();
+			$table->foreign('test_result_id')->references('id')->table('test_results');
+			$table->text('metadata');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('tests');
+	}
+
+}
