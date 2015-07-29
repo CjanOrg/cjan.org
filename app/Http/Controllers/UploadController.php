efa1192286ce4bc54aa32eb2b27b5a213a117785
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2015-07-29 22:13:31 +1200

    Working on the upload feature

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -1,7 +1,15 @@
 <?php namespace CJAN\Http\Controllers;
 
+use DB;
+use Exception;
 use Log;
-use Request;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+use CJAN\Models\ProjectGroupId;
+use CJAN\Models\ProjectArtifactId;
+use CJAN\Models\ProjectVersion;
 
 class UploadController extends Controller {
 
@@ -10,9 +18,50 @@ class UploadController extends Controller {
 
 	}
 
-	public function postResults()
+	public function postResults(Request $request)
 	{
-		Log::info(Request::all());
+		// FIXME: add access token and
+		// abort(403, 'Unauthorized action.');
+		$json = $request->input('json');
+		if ($json && strlen(trim($json)) > 0)
+		{
+			Log::debug(sprintf("JSON upload: %s", $json));
+
+			$jsonObject = json_decode($json);
+
+			try {
+				$groupId = $jsonObject->groupId;
+				$artifactId = $jsonObject->artifactId;
+				$version = $jsonObject->version;
+
+				$envProps = $jsonObject->envProps;
+				$status = $jsonObject->status;
+
+				$tests = $jsonObject->tests;
+
+				// FIXME: validate
+
+				DB::beginTransaction();
+
+				$projectGroupid = ProjectGroupId::firstOrCreate(['name' => $groupId]);
+				$projectArtifactId = ProjectArtifactId::firstOrCreate(['project_group_id_id' => $projectGroupid->id, 'name' => $artifactId, 'letter' => $artifactId[0]]);
+				$version = ProjectVersion::firstOrCreate(['project_artifact_id_id' => $projectArtifactId->id, 'name' => $version]);
+
+
+
+				DB::commit();
+
+				return (new Response(array('test_run' => 1), 200))
+	              ->header('Content-Type', 'application/json');
+	        } catch (Exception $e) {
+	        	DB::rollback();
+	        	Log::warning("Rolling back transaction: " . $e->getMessage());
+	        	return (new Response(array('error' => $e->getMessage()), 500))
+	              ->header('Content-Type', 'application/json');
+	        }
+		}
+
+		abort(500, "Invalid upload request");
 	}
 
 }
