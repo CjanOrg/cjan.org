e18fe621ba4dc7bdf18007b16b4ad87e87ffec9e
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2015-08-01 21:54:24 +1200

    Add getting started initial text

--- resources/views/getting_started.blade.php
+++ resources/views/getting_started.blade.php
@@ -10,39 +10,14 @@
 				<div class="panel-heading">Home</div>
 
 				<div class="panel-body">
-					<h4>Welcome to CJAN, the Comprehensive Java Archive Network</h4>
+					<h4>Getting started with CJAN</h4>
 
-					<p>The CJAN project is a placeholder for tests of Java projects. It aims at providing
-					better quality, less bugs in production, tests for alpha/beta/RC versions and history
-					of compatibility for Java projects.</p>
-					<p>Testers and developers can submit test results. Each result is archived alongside
-					with metadata such as: build tool version, project, version and operating system (OS
-					name, OS version and OS architecture).</p>
-					
-					<p>Read more about how to <a href="{{ url('getting-started') }}">get started</a>.</p>
+					<p>CJAN lists several Java projects and tests for each project in several platforms. The tests are uploaded
+					by users using the <a href="https://github.com/CjanOrg/test-collector-maven-plugin">CJAN Maven Plug-in</a>.</p>
 
-					<h4>Projects</h4>
-
-					<p>Choose a project by the first letter of its name, and browse the test results. Projects
-					are created by users when they upload test results. The project metadata is read from build
-					tools like Maven or Ant.</p>
-
-					<h4>Become a tester</h4>
-
-					<p>Several libraries are tested every day by many developers and testers. Not always
-					test results are archived and shared with others. By archiving and sharing your test
-					results, other developers can quickly evaluate the compatibility of libraries against
-					JVM, build tool and operating system versions.</p>
-
-					<p><a href="{{ URL::to('/auth/register') }}">Register</a> to create a new user and use our plug-in with your build
-					tool to submit test results. The information </p>
-
-					<h4>Contributions</h4>
-
-					<p>It is an Open Source project, licensed under the MIT License. The
-					<a href="https://github.com/CjanOrg/cjan.org">code</a> is hosted at GitHub. 
-					<a href="https://github.com/CjanOrg/CJAN/wiki/Roadmap">Contributions</a> are always
-					welcome!</p>
+					<p>The tests can be used by other developers for deciding whether they should use a library in their
+					application or not. If they know that they will be running the code in a platform with a JVM, locale or
+					timezone with failing tests, they may decide to use another library instead.</p>
 
 				</div>
 			</div>
