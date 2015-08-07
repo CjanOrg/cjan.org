d3199314faaa6b623f3d72def3f3af7456525e6a
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2015-08-07 23:52:28 +1200

    Envoy deploy script

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,5 +1,26 @@
 @servers(['web' => 'root@cjan.org'])
 
-@task('deploy')
- ls -la
+@macro('deploy')
+git
+composer
+fix-permissions
+@endmacro
+
+@task('git')
+cd /var/www/cjan.org/public_html
+git pull origin master
+@endtask
+
+@task('composer')
+cd /var/www/cjan.org/public_html
+composer install
+@endtask
+
+@task('migrate')
+cd /var/www/cjan.org/public_html
+php artisan migrate --seed
+@endtask
+
+@task('fix-permissions')
+chown -R www-data: /var/www/cjan.org/
 @endtask
