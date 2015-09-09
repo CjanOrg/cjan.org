feca0b67ebfaaf673c17f36aac139da2af4b6335
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2015-09-10 01:09:53 +1200

    Issue #11 menu item for tests in main menu

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -56,6 +56,7 @@
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
 							  <li><a href="{{ url('/auth/profile') }}">Profile</a></li>
+							  <li><a href="<?php echo url(sprintf('/u/%s/tests/', Auth::user()->name)) ?>">Tests</a></li>
 						      <li><a href="{{ url('/auth/logout') }}">Logout</a></li>
 							</ul>
 						</li>
